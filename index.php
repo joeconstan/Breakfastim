a6667e35789fddd169b65b74c9c80d76c0e7512e
ivorine85 <ido@bu.edu>
2016-10-28 18:26:14 -0400

    pass

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
 <form method="POST" action="login.php"id="form1">
     <input type="text" name="uname"> Username</input>
+	<input type="password" name="pass"> Password</input>
     <button type="submit" form="form1" value="Submit">Submit</button>
 </form>
 
