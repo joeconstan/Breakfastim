4c9ab31cd07e068c9e58b358d87bb182879d3c82
largepotatoes <jconstan@Bu.edu>
2016-10-28 18:41:06 -0400

    logindb

--- Login.php
+++ Login.php
@@ -1,7 +1,17 @@
 <?php
 
- if( $_POST["uname"] == "admin")
-     header('Location: second.php');
+    include 'dbconnect.php';
+    $connection = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $dbname);
+
+    if (!$connection){
+        die('could not connect:'.mysqli_connect_error());
+    }
+
+
+//    ( $_POST["uname"] == "admin")
+//    ( $_POST["pass"] == "admin")
+//take this all into a different file
+//    header('Location: second.php');
 
 
 ?>
