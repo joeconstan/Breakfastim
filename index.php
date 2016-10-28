a9da7661dada6c2e85317dbf57cc115d5155ad2c
largepotatoes <jconstan@Bu.edu>
2016-10-28 17:51:01 -0400

    files

---
+++ index.php
@@ -0,0 +1,23 @@
+<html><body>
+
+
+<?php
+    
+   if( $_POST["uname"] == "admin")
+        header('Location: second.php');
+
+?>
+
+
+<form method="POST" id="form1">
+    <input type="text" value="username" name="uname"></input>
+    
+</form>
+<button type="submit" form="form1" value="Submit">Submit</button>
+
+
+
+</body>
+
+
+</html>
---
+++ second.php
@@ -0,0 +1,12 @@
+<?php
+
+
+?>
+
+
+<html>
+<body>
+yay!
+</body>
+
+</html>
