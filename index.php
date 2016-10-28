30729f1444595dcc6f29c6054ef6e932c80a95d0
ivorine85 <ido@bu.edu>
2016-10-28 18:38:11 -0400

    head

--- index.php
+++ index.php
@@ -3,7 +3,30 @@
 <?php
 
 ?>
-<html><body>
+<html>
+
+	<head>
+		<title>Login</title>
+		
+		<style type = "text/css">
+			body {
+				font-family: Georgia, serif;
+				font-size: 16px;
+			}
+			
+			label {
+				font-weight: bold;
+				width: 100px;
+				font-size: 14px;
+			}
+			
+			.box {
+				border: #666666 solid 1px;
+			}
+		</style>
+	</head>
+
+<body>
 
 <form method="POST" action="login.php"id="form1">
     <input type="text" name="uname"> Username</input>
