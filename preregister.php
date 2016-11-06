ec2d137f2c73fd29211be928739187ee1fe99510
largepotatoes <jconstan@Bu.edu>
2016-11-06 17:19:46 -0500

    reg security

---
+++ preregister.php
@@ -0,0 +1,50 @@
+<?php
+
+?>
+
+<html>
+
+
+<br />
+<head>
+
+    <style type = "text/css">
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 16px;
+        }
+
+        label {
+            font-weight: bold;
+            width: 100px;
+            font-size: 14px;
+        }
+
+        .box {
+            border: #666666 solid 1px;
+        }
+    </style>
+</head>
+
+<body bgcolor = "#FFFFFF">
+
+<div align = "center">
+    <div style = "width:300px; border: solid 1px #333333; " align = "left">
+        <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+
+        <div style = "margin:30px">
+
+            <form method="POST" action="preregprocess.php" id="form1">
+                <input type="text" name="uname"> Username </input><br />
+                <input type="password" name="pass"> Password</input><br />
+                <button type="submit" class = "login_page" value="Submit">Login</button><br />
+            </form>
+        </div>
+    </div>
+
+</div>
+
+</body>
+
+
+</html>
---
+++ preregprocess.php
@@ -0,0 +1,36 @@
+<?php
+include 'dbconnect.php';
+$connection = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $dbname);
+
+if (!$connection){
+    die('could not connect:'.mysqli_connect_error());
+}
+
+
+$un = ( $_POST["uname"]);
+$pd = ( $_POST["pass"]);
+
+$query = "SELECT password FROM users WHERE username=?";
+$stmt = mysqli_stmt_init($connection);
+mysqli_stmt_prepare($stmt, $query);
+mysqli_stmt_bind_param($stmt, 's', $un);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_bind_result($stmt, $actualPass);
+
+
+$pswd = $_POST['pass'];
+mysqli_stmt_store_result($stmt);
+mysqli_stmt_fetch($stmt);
+$count = mysqli_stmt_num_rows($stmt);
+mysqli_stmt_close($stmt);
+
+if ($pswd == $actualPass && $count > 0){
+    header("location: http://www.breakfastim.com/register.php", true);
+    exit;
+}else{
+    echo 'Invalid Password';
+}
+
+mysqli_close($connection);
+
+?>
